b3914e61bae094f18d6bc40e6a9ae3ae508b5a7b
amcmullen9291 <amcmullen9291@gmail.com>
2021-09-20 22:12:25 -0400

    city info attributes

--- src/Components/Questions.js
+++ src/Components/Questions.js
@@ -3,15 +3,25 @@ import React, { useState, useEffect } from 'react';
 import { connect, useDispatch } from 'react-redux';
 import { setFactsList } from '../Actions/CityFacts'
 
-export default function Questions() {
+function Questions() {
 
         let bgcolor2 = document.getElementById("root");
         bgcolor2.style.backgroundColor = "blue";
 
+        const dispatch = useDispatch();
         let [facts, setFacts ] = useState([
-            
+            { id: "1", nameOf: "Westby", population: "2472", county: "Westby is located in Vernon County which is in south western Wisconsin.", meaning: "The name 'Westby' is a Norwegian name and literally translates to 'Western city'.", incorporated: "Westby was incorporated in 1896. The Norwegian flavor of the town is captured in the name of a store, Uff Da Shoppe.", zipcode: "Westby's zipcode is 54667.", areacode: "The city of Westby is located in the 608 area code.", festivals: "Westby's most popular event is the Syttende Mai festival, an annual three-day event held in mid-May. The festival stems from Westby's Norwegian roots, Syttende Mai being a Norwegian holiday ('Syttende Mai' translates to 'Seventeenth of May') celebrating Norway's independence and the signing of the Norwegian Constitution.", demographics: "The median age in the city was 41.5 years. 24.8% of residents were under the age of 18; 5.9% were between the ages of 18 and 24; 24.1% were from 25 to 44; 24.4% were from 45 to 64; and 20.8% were 65 years of age or older. The gender makeup of the city was 48.1% male and 51.9% female.", economy: "With seven cooperatives still functioning, Westby has been referred to as 'Co-op City'. Some of the more prominent co-ops are the Westby Cooperative Creamery, Vernon Telephone Cooperative, Heartland Country Coop, Westby Coop Credit Union, and Vernon Electric Cooperative."}
         ])
     
+        const setData = () => {
+            dispatch(setFactsList(facts));
+            console.log("Current Cities:", facts)
+          };
+          
+          useEffect(() => {
+            setData();
+          }, []);
+
   return (
       <>
     <div>
@@ -24,3 +34,18 @@ export default function Questions() {
     </>
   )
 }
+const mapStateToProps = (state) => {
+    return {
+      City: state.facts
+    }
+  
+  }
+  
+  const mapDispatchToProps = (dispatch) => {
+    return{
+      City: (facts) => { dispatch({type: 'SET_FACTS', facts})}
+    }
+  }
+
+  export default connect(mapStateToProps, mapDispatchToProps)(Questions);
+
--- src/Reducers/FactsReducer.js
+++ src/Reducers/FactsReducer.js
@@ -4,7 +4,7 @@ const initialState = {
     location: [],
 };
 
-export const IndexReducer = (state=initialState, {type, payload}) => {
+export const FactsReducer = (state=initialState, {type, payload}) => {
     switch (type) {
         case ActionTypes.SET_FACTS:
             return { ...state, location: payload};
